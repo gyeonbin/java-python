7f735d3ec06f54405e331242c590ad14c8eeb2fc
[gyeonbin] <[dkzkepal5017@gmail.com]>
2022-06-24 10:56:06 +0900

    ex0624

---
+++ java_work/ex0624/src/ex0624/AA.java
@@ -0,0 +1,20 @@
+package ex0624;
+
+public class AA {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		System.out.println("a = "+a);
+		
+		boolean b = true;
+		long c = 432145325123L;
+		System.out.println("b ="+b);
+		System.out.println("c ="+c);
+		
+		System.out.println(10+20);
+		int d = 444;
+		System.out.println(a+d);
+		
+	}
+}
