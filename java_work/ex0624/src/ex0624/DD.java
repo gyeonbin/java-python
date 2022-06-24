d4d8e078755abbfeac4420ce9cb7af5877a8d5e0
gyeonbin <108052593+gyeonbin@users.noreply.github.com>
2022-06-24 11:00:10 +0900

    Add files via upload

---
+++ java_work/ex0624/src/ex0624/DD.java
@@ -0,0 +1,21 @@
+package ex0624;
+import java.util.Scanner;
+
+public class DD {
+	
+	public static void main(String[] args) {
+		
+		final int AA = 10;
+		final String CC = "문자열";
+		
+		System.out.println("AA = "+AA);
+		System.out.println("CC = "+CC);
+		
+		System.out.println("안녕하세요\n");
+		System.out.println("안녕하세요\t김경빈입니다.\n");
+		System.out.println("\'안녕하세요\'");
+		System.out.println("\"bye my dear\"");
+		System.out.println("\\\\\\\\");
+		
+	}
+}
